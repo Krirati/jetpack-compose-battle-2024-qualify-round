6971d47236084d1d848eef5bb24558f0339298e2
krirati <kriratik@odds.team>
2024-01-22 14:51:02 +0700

    [Nine] Improve qualifiy 3 screen

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify3/Qualify3Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify3/Qualify3Screen.kt
@@ -1,13 +1,208 @@
 package com.github.thailandandroiddeveloper.common.ui.screen.qualify3
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
 @Composable
 fun Qualify3Screen() {
-    // TODO: Qualify3
+    Scaffold(
+        topBar = { AppBar() },
+        containerColor = MaterialTheme.colorScheme.onPrimary
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding)
+        ) {
+            Row(
+                modifier = Modifier
+                    .horizontalScroll(rememberScrollState())
+                    .padding(top = 16.dp)
+            ) {
+                CardItem(R.drawable.img_qualify_3_photo_1, true)
+                CardItem(R.drawable.img_qualify_3_photo_2, false)
+                CardItem(R.drawable.img_qualify_3_photo_3, false)
+            }
+            Tags()
+
+            for (i in 0..3) {
+                CardContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun CardItem(image: Int, withBorder: Boolean) {
+    Box(
+        modifier = Modifier
+            .padding(start =16.dp)
+            .size(160.dp, 320.dp)
+            .clip(RoundedCornerShape(16.dp))
+    ) {
+        Image(
+            modifier = Modifier
+                .border(
+                    width = if (withBorder) 2.dp else 0.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .clip(RoundedCornerShape(16.dp)),
+            painter = painterResource(id = image),
+            contentDescription = "image jone due",
+            contentScale = ContentScale.FillWidth
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Tags() {
+    val lists = listOf("Tag 1", "Tag 2", "Tag 3", "Tag 4")
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .padding(start = 16.dp)
+            .padding(vertical = 8.dp)
+    ) {
+        lists.forEach {
+            FilterChip(
+                modifier = Modifier.padding(end = 8.dp),
+                selected = it.contains("1"),
+                onClick = { },
+                label = { Text(it) },
+                colors = FilterChipDefaults.filterChipColors(
+                    labelColor = MaterialTheme.colorScheme.outline,
+                    selectedLabelColor = MaterialTheme.colorScheme.primary,
+                    selectedContainerColor = Color.Transparent
+                ),
+                border = FilterChipDefaults.filterChipBorder(
+                    selectedBorderColor = MaterialTheme.colorScheme.primary,
+                    selectedBorderWidth = 2.dp,
+                    borderColor = MaterialTheme.colorScheme.outline,
+                )
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+            Text(
+                "John Doe",
+                maxLines = 1,
+                style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.onPrimaryContainer)
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    painterResource(id = R.drawable.ic_qualify_3_back),
+                    contentDescription = "Back"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_qualify_3_copy),
+                    contentDescription = "copy button"
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_qualify_3_calendar),
+                    contentDescription = "calendar button"
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_qualify_3_menu),
+                    contentDescription = "menu button"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun CardContent() {
+    Box(
+        modifier = Modifier
+            .padding(bottom = 16.dp)
+            .padding(horizontal = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.surface)
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.surfaceVariant,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_qualify_3_sender),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+            )
+            Column(modifier = Modifier.padding(start = 8.dp)) {
+                Text(text = "Lorem Ipsum", style = MaterialTheme.typography.titleMedium)
+                Text(
+                    text = "Morbi sed sagittis justo, at pulvinar ipsum. Praesent massa metus, interdum at suscipit a, interdum vel orci. Pellentesque in sapien. Integer faucibus mauris ac luctus aliquam accumsan.",
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
 }
 
 // region Read-only because we use this to process your score.
