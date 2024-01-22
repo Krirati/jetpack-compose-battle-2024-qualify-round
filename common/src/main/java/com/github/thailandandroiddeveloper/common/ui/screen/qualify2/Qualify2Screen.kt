8a857067237b4408a12d8d4cc647a7b087bfe996
krirati <kriratik@odds.team>
2024-01-22 14:41:49 +0700

    [Nine] Improve qualifiy 2 screen

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
@@ -1,13 +1,152 @@
 package com.github.thailandandroiddeveloper.common.ui.screen.qualify2
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
 @Composable
 fun Qualify2Screen() {
-    // TODO: Qualify2
+    Scaffold(
+        topBar = { SkipButton() },
+        containerColor = MaterialTheme.colorScheme.onPrimary
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(contentPadding),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Lorem ipsum dolor sit amet",
+                    style = MaterialTheme.typography.headlineSmall
+                        .copy(color = MaterialTheme.colorScheme.onSurface)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dictum lacinia. Integer arcu neque, porttitor ac metus quis, iaculis molestie magna. Vivamus molestie justo sed nulla lacinia, quis fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla",
+                    style = MaterialTheme.typography.bodyMedium
+                        .copy(color = MaterialTheme.colorScheme.onSurface),
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+                Image(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(16.dp)),
+                    painter = painterResource(id = R.drawable.img_qualify_2_onboard),
+                    contentDescription = "",
+                    contentScale = ContentScale.FillWidth
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                DotIndicator()
+            }
+        }
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(color = MaterialTheme.colorScheme.primaryContainer)
+                    .height(102.dp)
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }, modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                        .fillMaxWidth()
+                        .padding(vertical = 31.dp, horizontal = 80.dp)
+                ) {
+                    Text(text = "Next")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SkipButton() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.End
+    ) {
+        TextButton(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp)) {
+            Text(text = "Skip")
+        }
+    }
+}
+
+@Composable
+fun DotIndicator() {
+    Row {
+        DotIndicatorInActive()
+        Spacer(modifier = Modifier.padding(end = 8.dp))
+        DotIndicatorInActive()
+        Spacer(modifier = Modifier.padding(end = 8.dp))
+        DotIndicatorInActive()
+        Spacer(modifier = Modifier.padding(end = 8.dp))
+        DotIndicatorActive()
+        Spacer(modifier = Modifier.padding(end = 8.dp))
+        DotIndicatorInActive()
+
+    }
+}
+
+@Composable
+fun DotIndicatorActive() {
+    Spacer(
+        modifier = Modifier
+            .width(32.dp)
+            .height(16.dp)
+            .clip(CircleShape)
+            .background(color = MaterialTheme.colorScheme.primary),
+    )
+}
+
+@Composable
+fun DotIndicatorInActive() {
+    Spacer(
+        modifier = Modifier
+            .size(16.dp)
+            .clip(CircleShape)
+            .background(color = MaterialTheme.colorScheme.primaryContainer),
+    )
 }
 
 // region Read-only because we use this to process your score.
