6c268e243270375fb2dfac69d4e2abb44bc78373
krirati <kriratik@odds.team>
2024-01-24 15:57:28 +0700

    [Nine] Improve qualifiy 2 update component

--- common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
+++ common/src/main/java/com/github/thailandandroiddeveloper/common/ui/screen/qualify2/Qualify2Screen.kt
@@ -3,155 +3,140 @@ package com.github.thailandandroiddeveloper.common.ui.screen.qualify2
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.github.thailandandroiddeveloper.common.R
 import com.github.thailandandroiddeveloper.common.ui.preview.Pixel7
 import com.github.thailandandroiddeveloper.common.ui.theme.AppTheme
 
 @Composable
 fun Qualify2Screen() {
-    Scaffold(
-        topBar = { },
-        containerColor = MaterialTheme.colorScheme.onPrimary
-    ) { contentPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .padding(contentPadding),
-        ) {
-            SkipButton()
-        }
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxHeight()
-        ) {
-            Box(modifier = Modifier.padding(16.dp)) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(38.dp),
-                    text = "Lorem ipsum dolor sit amet",
-                    style = MaterialTheme.typography.headlineSmall
-                        .copy(
-                            color = MaterialTheme.colorScheme.onSurface,
-                            textAlign = TextAlign.Center
-                        )
-                )
-            }
-            Box(modifier = Modifier.padding(horizontal = 32.dp)) {
-                Text(
-                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dictum lacinia. Integer arcu\n neque, porttitor ac metus quis, iaculis molestie magna. Vivamus molestie justo sed nulla lacinia, quis fringilla lorem imperdiet. Proin in quam vel odio  iaculis fringilla",
-                    style = MaterialTheme.typography.bodyMedium
-                        .copy(
-                            color = MaterialTheme.colorScheme.onSurface,
-                            textAlign = TextAlign.Center
-                        )
-                )
-            }
-            Box(modifier = Modifier.padding(31.dp)) {
-                Image(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(16.dp)),
-                    painter = painterResource(id = R.drawable.img_qualify_2_onboard),
-                    contentDescription = "",
-                    contentScale = ContentScale.FillWidth
-                )
-            }
-            Box(modifier = Modifier.padding(bottom = 48.dp)) {
-                DotIndicator()
-            }
-            StickyButton()
-        }
-    }
-}
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (button, title, body, image, stickyButton, indicator) = createRefs()
 
-@Composable
-private fun StickyButton() {
-    Column(
-        modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.primaryContainer)
-            .height(102.dp)
-            .padding(horizontal = 80.dp)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(
-            onClick = { /*TODO*/ }, modifier = Modifier
-                .fillMaxWidth()
-                .height(40.dp),
-            colors = ButtonDefaults.textButtonColors(
-                containerColor = MaterialTheme.colorScheme.primary,
-                contentColor = MaterialTheme.colorScheme.primary
-            )
-
-        ) {
-            Text(
-                text = "Next",
-                color = MaterialTheme.colorScheme.onPrimary,
-                style = MaterialTheme.typography.labelLarge
-            )
-        }
-    }
-}
-
-
-@Composable
-fun SkipButton() {
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.End
-    ) {
         TextButton(
             onClick = { /*TODO*/ },
-            modifier = Modifier
-                .padding(end = 6.dp, top = 6.dp),
-            contentPadding = PaddingValues(
-                start = 12.dp,
-                top = 10.dp,
-                end = 12.dp,
-                bottom = 10.dp,
-            )
+            shape = RectangleShape,
+            contentPadding = PaddingValues(top = 8.dp, end = 2.dp),
+            modifier = Modifier.constrainAs(button) { end.linkTo(parent.end, 8.dp) }
         ) {
-            Text(text = "Skip", style = MaterialTheme.typography.labelLarge)
+            Text(
+                text = "Skip",
+                style = MaterialTheme.typography.labelLarge.copy(color = MaterialTheme.colorScheme.primary),
+                textAlign = TextAlign.Center,
+                letterSpacing = .1.sp,
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(title) { top.linkTo(button.bottom, 16.dp) },
+            text = "Lorem ipsum dolor sit amet",
+            style = MaterialTheme.typography.headlineSmall
+                .copy(
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Center,
+                    letterSpacing = .5.sp,
+                ),
+        )
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dictum lacinia. Integer arcu neque, porttitor ac metus quis, iaculis molestie magna. Vivamus molestie justo sed nulla lacinia, quis fringilla lorem imperdiet. Proin in quam vel odio iaculis fringilla",
+            style = MaterialTheme.typography.bodyMedium
+                .copy(
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Center,
+                    letterSpacing = .5.sp,
+                ),
+            modifier = Modifier
+                .constrainAs(body) { top.linkTo(title.bottom, 16.dp) }
+                .padding(horizontal = 32.dp)
+        )
+        Column(modifier = Modifier
+            .constrainAs(image) {
+                top.linkTo(body.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+            .padding(top = 36.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .padding(horizontal = 32.dp),
+                painter = painterResource(id = R.drawable.img_qualify_2_onboard),
+                contentDescription = "onboard",
+            )
+        }
+        Column(modifier = Modifier.constrainAs(indicator) {
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(stickyButton.top)
+        }) {
+            DotIndicator()
+        }
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .height(102.dp)
+                .padding(horizontal = 80.dp)
+                .fillMaxWidth()
+                .constrainAs(stickyButton) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(251.dp)
+                    .height(40.dp),
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.primary
+                ),
+            ) {
+                Text(
+                    text = "Next",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = MaterialTheme.typography.labelLarge.copy(letterSpacing = .5.sp),
+                )
+            }
         }
     }
 }
 
 @Composable
 fun DotIndicator() {
-    Row {
+    Row(Modifier.padding(bottom = 48.dp, top = 32.dp)) {
         DotIndicatorInActive()
         Spacer(modifier = Modifier.padding(end = 8.dp))
         DotIndicatorInActive()
@@ -161,7 +146,6 @@ fun DotIndicator() {
         DotIndicatorActive()
         Spacer(modifier = Modifier.padding(end = 8.dp))
         DotIndicatorInActive()
-
     }
 }
 
